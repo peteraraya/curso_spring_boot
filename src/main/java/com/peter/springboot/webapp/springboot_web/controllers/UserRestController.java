ae0b4165bd43d100f8250c0b358c358fd6588cfc
Pedro Araya <piteraraya@gmail.com>
2026-07-15 19:55:04 -0400

    se actualiza UserRestController para manejar la ruta /details y retornar datos en formato JSON

--- src/main/java/com/peter/springboot/webapp/springboot_web/controllers/UserRestController.java
+++ src/main/java/com/peter/springboot/webapp/springboot_web/controllers/UserRestController.java
@@ -3,15 +3,17 @@ package com.peter.springboot.webapp.springboot_web.controllers;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/api")
 public class UserRestController {
     
-    @GetMapping("/details2")
+    @GetMapping("/details")
 
     public Map<String, Object> details(Model model) {
       Map<String, Object> body = new HashMap<>();
