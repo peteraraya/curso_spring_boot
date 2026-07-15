821dee670d46f2a874e3d8da66ab89db19c72d31
Pedro Araya <piteraraya@gmail.com>
2026-07-15 19:45:19 -0400

    se actualiza UserController y detalles.html para incluir atributos de modelo en la vista

--- src/main/java/com/peter/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/peter/springboot/webapp/springboot_web/controllers/UserController.java
@@ -1,15 +1,18 @@
 package com.peter.springboot.webapp.springboot_web.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
     
     @GetMapping("/")
-    
-    public String details() {
 
+    public String details(Model model) {
+      model.addAttribute("title", "hola como estas");
+      model.addAttribute("name", "Pedro");
+      model.addAttribute("lastName", "Araya");
       return  "details";
     }
 }
