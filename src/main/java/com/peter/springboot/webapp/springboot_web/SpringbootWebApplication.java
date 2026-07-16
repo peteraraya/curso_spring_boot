abe49b6fdf088a8b9d2320f4679cc18ef15d2ef4
Pedro Araya <piteraraya@gmail.com>
2026-07-16 17:40:48 -0400

    se agrega un constructor en UserRestController para inyección de dependencias y se implementa un nuevo endpoint /api/list para devolver una lista de usuarios

--- src/main/java/com/peter/springboot/webapp/springboot_web/SpringbootWebApplication.java
+++ src/main/java/com/peter/springboot/webapp/springboot_web/SpringbootWebApplication.java
@@ -3,9 +3,17 @@ package com.peter.springboot.webapp.springboot_web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Clase principal que inicializa la aplicación Spring Boot.
+ */
 @SpringBootApplication
 public class SpringbootWebApplication {
 
+	/**
+	 * Método principal que sirve como punto de entrada de la aplicación.
+	 * 
+	 * @param args Argumentos de línea de comandos.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootWebApplication.class, args);
 	}
--- src/main/java/com/peter/springboot/webapp/springboot_web/controllers/UserRestController.java
+++ src/main/java/com/peter/springboot/webapp/springboot_web/controllers/UserRestController.java
@@ -1,6 +1,7 @@
 package com.peter.springboot.webapp.springboot_web.controllers;
 
 
+import com.peter.springboot.webapp.springboot_web.SpringbootWebApplication;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.peter.springboot.webapp.springboot_web.models.User;
 import com.peter.springboot.webapp.springboot_web.models.dto.UserDto;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 /**
  * Controlador REST que maneja las peticiones relacionadas con los usuarios.
@@ -21,6 +27,17 @@ import java.util.Map;
 @RequestMapping("/api")
 public class UserRestController {
     
+    private final SpringbootWebApplication springbootWebApplication;
+
+    /**
+     * Constructor para inyección de dependencias.
+     * 
+     * @param springbootWebApplication Instancia de la aplicación principal.
+     */
+    UserRestController(SpringbootWebApplication springbootWebApplication) {
+      this.springbootWebApplication = springbootWebApplication;
+    }
+
     /**
      * Endpoint que proporciona los detalles del usuario utilizando un DTO.
      * Mapea a la ruta /api/details utilizando el método HTTP GET.
@@ -39,6 +56,32 @@ public class UserRestController {
       // Retorna el objeto UserDto que Spring convertirá automáticamente a formato JSON
       return userDto;
     }
+
+    /**
+     * Endpoint que proporciona una lista de usuarios.
+     * Mapea a la ruta /api/list utilizando el método HTTP GET.
+     * 
+     * @return Lista de objetos User que se convertirá automáticamente a formato JSON.
+     */
+    @GetMapping("/list")
+    public List<User> list() {
+        User user = new User("Pedro", "Araya");
+        User user2 = new User("John", "Parra");
+        User user3 = new User("Benja", "Diaz");
+        User user4 = new User("Juan Pablo", "Leon");
+
+        // HelperspringbootWebApplication
+        List<User> users = Arrays.asList(user,user2,user3,user4);
+
+        // List<User> users = new ArrayList<>();
+
+        // users.add(user);
+        // users.add(user2);
+        // users.add(user3);
+        // users.add(user4);
+
+        return users;
+      }   
 
     /**
      * Endpoint que proporciona los detalles de un usuario utilizando un Map.
